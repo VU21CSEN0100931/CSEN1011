cd35990d03b7a801da55e0c786c75814b9580446
AKSHITH <93367840+VU21CSEN0100931@users.noreply.github.com>
2021-11-26 12:05:33 +0530

    Update average of 5 subjects.cpp

--- average of 5 subjects.cpp
+++ average of 5 subjects.cpp
@@ -5,11 +5,9 @@ int main()
     float eng, phy, chem, math, comp; 
     float total, average, percentage;
 
-    /* Input marks of all five subjects */
     printf("Enter marks of five subjects: \n");
     scanf("%f%f%f%f%f", &eng, &phy, &chem, &math, &comp);
 
-    /* Calculate total, average and percentage */
     total = eng + phy + chem + math + comp;
     average = total / 5.0;
     percentage = (total / 500.0) * 100;
