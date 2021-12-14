cb883bcfcfeef0b071afdf03b1ee0a79df348339
AKSHITH <93367840+VU21CSEN0100931@users.noreply.github.com>
2021-12-14 09:14:14 +0000

    c programe

---
+++ reversenumber.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int a,b=0,c;
+	printf("Enter a number");
+	scanf("%d",&a);
+	while(a!=0)
+	{
+		c=a%10;
+		b=b*10+c;
+		a/=10;
+	}
+	printf("reversed number:%d",b);
+	return 0;
+}
