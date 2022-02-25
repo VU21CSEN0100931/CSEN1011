592e1d1bae00a2785d4faebc7dbb8121ad1ffb4c
AKSHITH <93367840+VU21CSEN0100931@users.noreply.github.com>
2022-02-25 12:30:41 +0530

    Add files via upload

---
+++ chess board.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+	
+int a,k,s,h,sum;
+
+scanf("%d%d%d%d",&a,&k,&s,&h);
+
+sum=a+k+s+h;
+
+if(sum%2==0)
+
+{
+printf("True");
+
+}
+else
+{
+printf("False");
+
+}
+}
